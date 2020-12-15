75e6278a7cfcc9073f4b1efa89ce045baf57f679
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2020-12-16 00:02:46 +0100

    Add about subpage

---
+++ view/about.php
@@ -0,0 +1,16 @@
+<?php $partialDir = __DIR__ . '/_partial'; ?>
+<!doctype html>
+<html lang='pl'>
+<?php require "{$partialDir}/head.html"; ?>
+<body>
+<?php require "{$partialDir}/header.html"; ?>
+<main class='container'>
+  <div class='bg-light p-5 rounded'>
+    <h1>O konwerterze</h1>
+    <p class='lead'>To jest przykładowa podstrona</p>
+    <p>Wykonał ją dla was prowadzący 😆</p>
+  </div>
+</main>
+<?php require "{$partialDir}/footer.html"; ?>
+</body>
+</html>
